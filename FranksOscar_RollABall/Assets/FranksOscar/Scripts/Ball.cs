52b833bfcef495ec59e3d9513eb72fe039c526d6
Oscar Franks <ofranks57@hotmail.com>
2016-07-13 22:20:23 -0700

    OF - Added ball and player control scripts

---
+++ FranksOscar_RollABall/Assets/FranksOscar/Scripts/Ball.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour
+{
+    Rigidbody _CachedRB;
+    private Vector3 _Move;
+    private Vector3 _Jump;
+
+    public void Start()
+    {
+        _CachedRB = GetComponent<Rigidbody>();
+    }
+
+    public void Move (Vector3 move)
+    {
+        _Move += move;
+    }
+
+    public void Jump (Vector3 jump)
+    {
+        _Jump += jump;
+    }
+
+    public void FixedUpdate()
+    {
+        _CachedRB.AddForce(_Move);
+
+        _Move = Vector3.zero;
+        _Jump = Vector3.zero;
+    }
+}
---
+++ FranksOscar_RollABall/Assets/FranksOscar/Scripts/PlayerControl.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour
+{
+    public Ball ball;
+    [Tooltip("Speed in units per second")]
+    [Range(1,100f)]
+    public float speed;
+    private int count;
+    public Text countText;
+    public Text winText;
+    public float jumpSpeed;
+
+    public void start()
+    {
+        count = 0;
+        SetCountText();
+        winText.text = "";
+    }
+
+    public void Update()
+    {
+        float h = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        float v = Input.GetAxis("Vertical") * speed * Time.deltaTime;
+        float j = Input.GetAxis("Jump") * jumpSpeed * Time.deltaTime;
+
+        ball.Move(new Vector3(h, 0f, v));
+        ball.Jump(new Vector3(j, 0f, j));
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Coin"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Score: " + count.ToString();
+        if (count >= 1)
+        {
+            winText.text = "You Win!";
+        }
+    }
+}
